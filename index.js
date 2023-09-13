2765a071c37144c8f9cd571dfeb37c4c42d602ce
rajroushan6714 <rajubhai6714x@gmail.com>
2023-09-13 12:17:49 +0530

    Minor modification to the project, if the player selects the wrong choice

--- index.js
+++ index.js
@@ -2,56 +2,97 @@
   paper = 1
   scissor = 2
   */
+
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay(computerChoice){
+
     let c = Math.random() * 3;
     c = Math.floor(c);
+
     if(c == 0){
         computerChoice.value = "rock";
     }
+
     else if(c == 1){
         computerChoice.value = "paper";
     }
+
     else if(c == 2){
         computerChoice.value = "scissors";
     }
+
     console.log(`Computer choice: ${computerChoice.value}`);
+
     return c;
+
 }
+
 function playRound(playerChoiceInt, playerChoice, computerChoice , computerChoiceInt){
+
     let win_Array = [[0, 2, 1],
                      [1, 0, 2],
                      [2, 1, 0]];
+
     let result = win_Array[playerChoiceInt][computerChoiceInt];
+
     if(result == 0){
-        console.log(`It's a tie! You choose: ${playerChoice} , and The Computer choose: ${computerChoice.value}`);
+        return `It's a tie! You choose: ${playerChoice} , and The Computer choose: ${computerChoice.value}`;
     }
+
     else if(result == 1){
-        console.log(`You Won! You choose: ${playerChoice} , and The Computer choose: ${computerChoice.value}`)
+        playerScore++;
+        return `You Won! You choose: ${playerChoice} , and The Computer choose: ${computerChoice.value}`
     }
+
     else if(result == 2){
-        console.log(`You Lost! You choose: ${playerChoice} , and The Computer choose: ${computerChoice.value}`);
+        computerScore++;
+        return `You Lost! You choose: ${playerChoice} , and The Computer choose: ${computerChoice.value}`;
     }
-    console.log("\n");
+
+    else {
+        return "No Result";
+    }
+
 }
 
 function game(){
+
     let computerChoice = {value : ""};
     let computerChoiceInt ;
     let playerChoice;
     let playerChoiceInt;
 
     for(let i = 1 ; i <= 5 ; i++){
+
         playerChoice = prompt("Please enter your choice: ").toLowerCase();
+
         if(playerChoice == "rock"){
             playerChoiceInt = 0;
         }
+
         else if(playerChoice == "paper"){
             playerChoiceInt = 1;
         }
+
         else if(playerChoice == "scissors"){
             playerChoiceInt = 2;
         }
+        else{
+            console.log("Wrong Choice");
+            console.log(`Player Score: ${playerScore}`);
+            console.log(`Computer Score: ${computerScore}`);
+            console.log("\n");
+            continue;
+        }
         computerChoiceInt = computerPlay(computerChoice);
-        playRound(playerChoiceInt, playerChoice, computerChoice , computerChoiceInt);
+
+        console.log(playRound(playerChoiceInt, playerChoice, computerChoice , computerChoiceInt));
+
+        console.log(`Player Score: ${playerScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+        console.log("\n");
+
     }
 }
